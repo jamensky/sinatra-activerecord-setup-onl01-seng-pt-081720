9e1637c51ad56ce744d5e009dc114f64486dd53c
Jamal Rjoul <jamensky1991@gmail.com>
2020-11-30 20:30:06 +0000

    Automatically backed up by Learn

--- db/migrate/20201130202435_create_dogs.rb
+++ db/migrate/20201130202435_create_dogs.rb
@@ -1,8 +1,9 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def up#creates table and collumns
-    create_table :dogs |t|
-    t.string :name
-    t.string :breed
+     create_table :dogs do |t|
+     t.string :name
+     t.string :breed
+    end
   end
 
   def down#drops the table
